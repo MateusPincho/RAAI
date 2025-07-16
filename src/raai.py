06dc2ee200a871812d6d7a496ea30d96f9ddb371
Mateus Pincho <mateus.oliveira@ee.ufcg.edu.br>
2025-07-15 21:12:26 -0300

    modelagem da classe de RAAI, que representa um esqueleto para o desenvolvimento inicial do código do robô

---
+++ src/raai.py
@@ -0,0 +1,12 @@
+class RAAI:
+    def __init__(self):
+        pass
+
+    def ler_sensores(self):
+        pass
+
+    def navegar(self):
+        pass
+
+    def desligar_motores(self):
+        pass
